0e3b7afe8ef952aca813edcdd25ce65f37eae1bf
Ravi Sharma <rsharma@kitchen.local>
2025-01-03 20:58:38 +0000

    algonomics:chaldean: Claldean numerology
    Added support for command line and now rusty return total of
    all chaldeans alpha-numero

--- algonomics/chaldean/src/main.rs
+++ algonomics/chaldean/src/main.rs
@@ -1,4 +1,4 @@
-
+use std::env;
 
 fn chaldean_lookupv1(v: &Vec<Vec<char>>, x: char) -> u32 {
 
@@ -7,8 +7,9 @@ fn chaldean_lookupv1(v: &Vec<Vec<char>>, x: char) -> u32 {
     for (v, i) in v.iter().enumerate() {
         let b = i.contains(&x);
         match b {
-            true => { let r = i.binary_search(&x); match(r) {Ok(index) => {println!("macthed {}", index);idx = v as u32; break}, Err(insert_pos) => println!("error! not found")}  }, 
-            _ => println!("next vector please {}", v),    
+            /*true => { let r = i.binary_search(&x); match r {Ok(index) => {/*dbg!("matched {}", index);*/idx = v as u32; break}, Err(insert_pos) => println!("error! not found")}  },*/ 
+            true => { idx = v as u32}, 
+            _ => {}, //println!("next vector please {}", v),    
         }
     }
     idx
@@ -20,7 +21,7 @@ fn chaldean_lookup(v: &Vec<Vec<char>>, x: char) -> u32 {
 
     for (v, i) in v.iter().enumerate() {
         for j in i {
-            println!("lookup for {} {} {} {:?}", x, j, v, i);
+            //println!("lookup for {} {} {} {:?}", x, j, v, i);
             match j {
                 val if *val == x => {println!("matched"); idx = v as u32; break},
                 _ => println!("not in this vector {} {}", v, x),
@@ -32,16 +33,28 @@ fn chaldean_lookup(v: &Vec<Vec<char>>, x: char) -> u32 {
 fn chaldean_sum(v: &Vec<Vec<char>>, name: &String) -> u32 {
 
     let mut tot = 0;
+    let mut x: u32 = 0;
 
     for x in name.chars() {
     let idx = chaldean_lookupv1(&v, x);
-    println!("chaldean look up for {} {}", x, idx);
+    //println!("chaldean look up for {} {}", x, idx);
     tot += idx;
     }
-    return tot;
+
+    let digits: Vec<String> = tot.to_string().chars().map(|c| c.to_string()).collect();
+    for d in digits {
+        match d.parse::<u32>() {
+            Ok(a) => x += a,
+            Err(e) => {},
+        }
+    }
+    return x;
 }
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
 
     let mut chaldean  = vec![vec!['_'],
                          vec!['a', 'q', 'y', 'i', 'j'],
@@ -58,7 +71,6 @@ fn main() {
     //chaldean.sort();
 
     for (v,i) in chaldean.iter_mut().enumerate() {
-
         i.sort();
             print!("{:?} -> ", v);
         for j in i {
@@ -72,10 +84,8 @@ fn main() {
     ];
     print!(" {:?} ", _chaldean);
 
-    let test = String::from("ravi");
-
-    println!("name {:?} total {}", test, chaldean_sum(&chaldean, &test));
-
-
+    let test = String::from("poomale");
+    let name = &args[1];
+    println!("chaldean totla for name {:?} => {}",&name, chaldean_sum(&chaldean, &name));
 
 }
