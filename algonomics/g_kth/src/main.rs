d9be7f6848e36fedb1a5876598c0d23599d1873c
Ravi Sharma <rsharma@kitchen.local>
2025-01-05 19:29:31 +0000

    algonomics:g_kth Kth problem inefficient solution
    stream and sorted clone of stream.
    beautified code

--- algonomics/g_kth/src/main.rs
+++ algonomics/g_kth/src/main.rs
@@ -1,13 +1,13 @@
 /*Find the kth largest element in a number stream
-    Design a class to efficiently find the kth  largest element in a stream of numbers. 
-    The class should have the following two things
-    1.The constructor of the class should accept an integer array (nums) containing initial numbers from the stream and an integer kth. 
-    2.The class should expose a function add(int value) which will add the number value to the stream of numbers, and will return the kth largest number in the stream seen so far.
-    Example:
-    Input: [4, 1, 3, 12, 7, 14], k = 3
-    Calling add(6) should return '7'.
-    Calling add(13) should return '12'.
-    Calling add(4) should still return '12'.
+Design a class to efficiently find the kth  largest element in a stream of numbers.
+The class should have the following two things
+1.The constructor of the class should accept an integer array (nums) containing initial numbers from the stream and an integer kth.
+2.The class should expose a function add(int value) which will add the number value to the stream of numbers, and will return the kth largest number in the stream seen so far.
+Example:
+Input: [4, 1, 3, 12, 7, 14], k = 3
+Calling add(6) should return '7'.
+Calling add(13) should return '12'.
+Calling add(4) should still return '12'.
 */
 
 #[derive(Debug)]
@@ -18,20 +18,20 @@ struct Heap {
 }
 
 impl Heap {
-    fn new(arr: &[u32], k: u32) ->  Heap {
+    fn new(arr: &[u32], k: u32) -> Heap {
         let mut c = arr.to_vec();
         c.sort();
         Heap {
-        list: arr.to_vec(),
-        clone:c,
-        k,
+            list: arr.to_vec(),
+            clone: c,
+            k,
         }
     }
 
     fn add(&mut self, val: u32) -> u32 {
         let mut kth = 0;
         self.list.push(val);
-        for (i, &x) in self.clone.iter().enumerate()  {
+        for (i, &x) in self.clone.iter().enumerate() {
             if val < x {
                 println!(" x {} > val {} @ {}", x, val, i);
                 self.clone.insert(i, val);
@@ -45,7 +45,7 @@ impl Heap {
                 kth = *x as u32;
                 break;
             }
-            iter-=1;
+            iter -= 1;
         }
         kth
     }
@@ -54,7 +54,7 @@ impl Heap {
 fn main() {
     println!("kth problem!");
 
-    let arr:[u32; 6] = [4, 1, 3, 12, 7, 14];
+    let arr: [u32; 6] = [4, 1, 3, 12, 7, 14];
     let mut v: Heap = Heap::new(&arr, 3);
     println!("{:? }", v);
     println!("add and return {}", v.add(6));
