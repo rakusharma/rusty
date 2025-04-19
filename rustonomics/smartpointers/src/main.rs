b93f7cf2b0e76736c9e9062e8330db267cefa0dc
Ravi Sharma <rsharma@kitchen.local>
2025-04-19 11:25:36 +0100

    rusty: smartpointers
    basic of Rc, RcCell covered.
    closure added as pinch of salt

---
+++ rustonomics/smartpointers/src/main.rs
@@ -0,0 +1,49 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    //On stack
+    let y: u32 = 5;
+    //On heap & non-mutable reference
+    let x = Rc::new(5);
+    let u = Rc::clone(&x);
+    let v = Rc::clone(&x);
+    //below do the ownership tranferring, so not allowed
+    //dbg!(x);
+    //allowed
+    dbg!(&x);
+    dbg!(Rc::strong_count(&x));
+
+    //Now lets have a  rc with mutable
+    // first new Rc pointer will be created to track the Vec using reference count
+    //Read only data immutable
+    let x: Rc<Vec<u32>> = Rc::new(vec![1, 2, 0]);
+    let read_only = Rc::clone(&x);
+
+    //let's make vector mutable
+    let immutable_rc: Rc<RefCell<Vec<u32>>> = Rc::new(RefCell::new(vec![1, 2, 3]));
+    let ex1 = immutable_rc.clone();
+    dbg!(&ex1);
+    //Mutate the vector
+    immutable_rc.borrow_mut().push(4);
+    dbg!(&immutable_rc);
+    dbg!(&ex1);
+    //Hurray below will panic!!!
+    let test = immutable_rc.borrow_mut();
+    //uncomment below to panic
+    //dbg!(immutable_rc.borrow());
+
+    let a = 45;
+    let b = 78;
+    let max = |a, b| {
+        if a > b {
+            a
+        } else {
+            b
+        }
+    };
+    /*wow nested closures*/
+    let test_max = |a, b| println!("max : {:?}", max(a, b));
+
+    test_max(a, b);
+}
