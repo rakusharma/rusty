0fef72cc4d1b50d4f262044e21a225aaa3442963
Ravi Sharma <sharmaravi.in@gmail.com>
2025-06-05 09:42:52 +0100

    permutations
    Added support for non-recursive permutation for u32
    changes simple functions to traits
    
    Signed-off-by: Ravi Sharma <sharmaravi.in@gmail.com>

--- rustonomics/permutation/src/main.rs
+++ rustonomics/permutation/src/main.rs
@@ -1,35 +1,44 @@
-use std::collections::linked_list;
+trait Permutation {
+    fn permutations(&self, list: Vec<u32>) -> Vec<Vec<u32>>;
+    fn permutations_recursive(&self, list: Vec<u32>, path: Vec<u32>);
+}
 
-fn permutations(list: Vec<u32>) {
-    let mut stack: Vec<Vec<u32>> = Vec::new();
-    let mut path: Vec<Vec<u32>> = vec![];
+struct NumberPermutations;
 
-    stack.push(vec![]);
+impl Permutation for NumberPermutations {
+    fn permutations(&self, list: Vec<u32>) -> Vec<Vec<u32>> {
+        let mut stack: Vec<Vec<u32>> = Vec::new();
+        let mut path: Vec<Vec<u32>> = vec![];
+        /*key is pushing items as vector and pop each and add items to it */
 
-    while !stack.is_empty() {
-        if let Some(temp) = stack.pop() {
-            if temp.len() == list.len() {
-                path.push(temp.clone());
-                continue;
-            }
-            for i in &list {
-                if temp.contains(&i) {
+        stack.push(vec![]);
+
+        while !stack.is_empty() {
+            if let Some(temp) = stack.pop() {
+                if temp.len() == list.len() {
+                    path.push(temp.clone());
                     continue;
                 }
-
-                let mut new_path = if temp.is_empty() {
-                    Vec::new()
-                } else {
-                    temp.clone()
-                };
-                new_path.push(*i);
-                stack.push(new_path.clone());
+                for i in list.iter().rev() {
+                    if temp.contains(i) {
+                        continue;
+                    }
+                    let mut new_path = if temp.is_empty() {
+                        Vec::new()
+                    } else {
+                        temp.clone()
+                    };
+                    new_path.push(*i);
+                    stack.push(new_path.clone());
+                }
             }
         }
+        path
     }
-    println!("{:?}", path);
+    fn permutations_recursive(&self, list: Vec<u32>, path: Vec<u32>) {}
 }
 fn main() {
+    let perms = NumberPermutations;
     let list: Vec<u32> = vec![1, 2, 3];
-    permutations(list);
+    println!("{:?}", perms.permutations(list));
 }
